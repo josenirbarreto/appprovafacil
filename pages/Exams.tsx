57b20b0db1b2aeb36527b7fb020bac1650aefb71
josenirbarreto <josenirbarreto@gmail.com>
2025-12-22 18:59:46 -0300

    Add files via upload

--- pages/Exams.tsx
+++ pages/Exams.tsx
@@ -419,12 +419,11 @@ const ExamsPage = () => {
                         </div>
                     </div>
 
-                    <div className="lg:col-span-2 bg-white rounded-2xl p-4 border border-slate-200 min-h-[600px] overflow-y-auto custom-scrollbar print:shadow-none print:border-none print:p-0">
+                    <div className="lg:col-span-2 bg-white rounded-2xl p-4 border border-slate-200 overflow-y-auto custom-scrollbar print:shadow-none print:border-none print:p-0">
                         {/* 
                             O ID 'exam-print-container' é alvo do CSS de impressão para garantir fluxo contínuo.
-                            Removidos paddings superiores de preview durante a impressão real.
                         */}
-                        <div id="exam-print-container" className={`${printFontSize} text-black bg-white w-full`}>
+                        <div id="exam-print-container" className={`${printFontSize} text-black bg-white w-full print:p-0`}>
                             {viewingMode === 'EXAM' ? (
                                 <div className="animate-fade-in bg-white w-full">
                                     {renderHeaderPrint()}
